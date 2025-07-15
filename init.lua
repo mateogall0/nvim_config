0bd9d60c7336758035f4ed609d3769b319096fb0
mateogall0 <mateogesede@gmail.com>
2025-07-15 19:03:54 -0300

    wip: modify exit prompt

--- init.lua
+++ init.lua
@@ -1,99 +1,103 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.termguicolors = true
-vim.o.clipboard = "unnamedplus"
-vim.o.cursorline = true
-vim.o.splitbelow = true
-vim.o.splitright = true
--- Keymaps
-vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true }) -- Open file explorer
-vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true }) -- Save file
+  vim.o.number = true
+  vim.o.relativenumber = true
+  vim.o.termguicolors = true
+  vim.o.clipboard = "unnamedplus"
+  vim.o.cursorline = true
+  vim.o.splitbelow = true
+  vim.o.splitright = true
+  -- Keymaps
+  vim.keymap.set("n", "<leader>e", ":Ex<CR>", { noremap = true }) -- Open file explorer
+  vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true }) -- Save file
 
--- Plugin management
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", lazypath
+  -- Plugin management
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git", "clone", "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git", lazypath
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
+
+  require("lazy").setup({
+    -- File explorer
+    { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
+    
+    -- Fuzzy finder
+    { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
+    
+    -- LSP & Autocompletion
+    { "neovim/nvim-lspconfig" },
+    { "hrsh7th/nvim-cmp", dependencies = {
+      "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"
+    }},
+
+    -- Treesitter for better syntax highlighting
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+
+    -- Status line
+    { "nvim-lualine/lualine.nvim" },
+
+    -- Theme
+    { "folke/tokyonight.nvim" }
   })
-end
-vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  -- File explorer
-  { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
-  
-  -- Fuzzy finder
-  { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
-  
-  -- LSP & Autocompletion
-  { "neovim/nvim-lspconfig" },
-  { "hrsh7th/nvim-cmp", dependencies = {
-    "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"
-  }},
-
-  -- Treesitter for better syntax highlighting
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-  -- Status line
-  { "nvim-lualine/lualine.nvim" },
-
-  -- Theme
-  { "folke/tokyonight.nvim" }
-})
-
--- Setup plugins
-require("nvim-web-devicons").setup()
-require("nvim-tree").setup()
-require("lualine").setup({ options = { theme = "tokyonight" } })
-require("telescope").setup()
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "python", "javascript", "bash",
-    "c", "cpp", "gdscript", "godot_resource", "gdshader","rust", "java",
-    "c_sharp", "go", "gdscript", "html", "css", "json", "yaml",
-    "markdown", "markdown_inline", "toml", "vim", "query", "regex",
-    "nasm", "glsl", "wgsl", "dart" },
-  highlight = { enable = true },
-  indent = { enable = true },
-  auto_install = true,
-})
-
--- Completion setup
-local cmp = require("cmp")
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping.select_next_item(),
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true })
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" }
+  -- Setup plugins
+  require("nvim-web-devicons").setup()
+  require("nvim-tree").setup()
+  require("lualine").setup({ options = { theme = "tokyonight" } })
+  require("telescope").setup()
+  require("nvim-treesitter.configs").setup({
+    ensure_installed = { "lua", "python", "javascript", "bash",
+      "c", "cpp", "gdscript", "godot_resource", "gdshader","rust", "java",
+      "c_sharp", "go", "gdscript", "html", "css", "json", "yaml",
+      "markdown", "markdown_inline", "toml", "vim", "query", "regex",
+      "nasm", "glsl", "wgsl", "dart" },
+    highlight = { enable = true },
+    indent = { enable = true },
+    auto_install = true,
   })
-})
 
-vim.keymap.set('n', '<F2>', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+  -- Completion setup
+  local cmp = require("cmp")
+  cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+      ["<Tab>"] = cmp.mapping.select_next_item(),
+      ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true })
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "buffer" },
+      { name = "path" }
+    })
+  })
 
-local lspconfig = require('lspconfig')
-lspconfig.pyright.setup{}
-vim.cmd.colorscheme("tokyonight")
+  vim.keymap.set('n', '<F2>', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+
+  local lspconfig = require('lspconfig')
+  lspconfig.pyright.setup{}
+  vim.cmd.colorscheme("tokyonight")
 
 
 vim.api.nvim_create_user_command("C", function(opts)
   local cmd = table.concat(opts.fargs, " ")
   local start_time = vim.loop.hrtime()
 
-  vim.cmd("botright split | resize 20 | terminal " .. cmd)
+  local tagged_cmd = string.format(
+    "bash -c '%s; echo EXIT:$?; read -n 1 -s -r -p \"Press any key to exit...\"'",
+    cmd
+  )
+  vim.cmd("botright split | resize 20 | terminal " .. tagged_cmd)
 
   local term_win = vim.api.nvim_get_current_win()
   local term_buf = vim.api.nvim_get_current_buf()
-  -- Move cursor to bottom of terminal buffer (Shift+G)
+
   vim.api.nvim_buf_call(term_buf, function()
     vim.cmd("normal! G")
   end)
-  -- Auto-close on WinLeave
+
   vim.api.nvim_create_autocmd("WinLeave", {
     buffer = term_buf,
     once = true,
@@ -104,28 +108,38 @@ vim.api.nvim_create_user_command("C", function(opts)
     end,
   })
 
-  -- Print exit code and duration
-  vim.api.nvim_create_autocmd("TermClose", {
-buffer = term_buf,
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    buffer = term_buf,
     once = true,
-    callback = function(args)
+    callback = function()
       local elapsed = (vim.loop.hrtime() - start_time) / 1e9
-      local exit_code = args.data or 0
+      local lines = vim.api.nvim_buf_get_lines(term_buf, 0, -1, false)
+
+      local exit_code = 0
+      for i = #lines, 1, -1 do
+        local match = lines[i]:match("^EXIT:(%d+)$")
+        if match then
+          exit_code = tonumber(match)
+          break
+        end
+      end
 
       vim.schedule(function()
-        print(string.format("[Process exited %d] in %.2f seconds", exit_code, elapsed))
+        local msg = string.format("[Process exited %d] in %.2f seconds", exit_code, elapsed)
+        local hl = exit_code == 0 and "DiffAdd" or "ErrorMsg"
+        vim.api.nvim_echo({{msg, hl}}, false, {})
       end)
     end,
   })
 end, { nargs = "+" })
 
--- Quick tab switch
-vim.keymap.set("n", "Z", ":tab split<CR>", { noremap = true })
+  -- Quick tab switch
+  vim.keymap.set("n", "Z", ":tab split<CR>", { noremap = true })
 
--- Don't create a swap file
-vim.o.swapfile = false
+  -- Don't create a swap file
+  vim.o.swapfile = false
 
-vim.cmd([[
-  autocmd FileType dart,javascript,typescript,json,yaml,html,css,lua,graphql setlocal shiftwidth=2 tabstop=2 expandtab
-]])
+  vim.cmd([[
+    autocmd FileType dart,javascript,typescript,json,yaml,html,css,lua,graphql setlocal shiftwidth=2 tabstop=2 expandtab
+  ]])
 
